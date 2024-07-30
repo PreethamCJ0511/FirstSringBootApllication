eba69e2157b3f8daefb6edd4000a0ef0d7de7ca1
Preetham <cjpreetham@gmail.com>
2024-07-30 17:51:02 +0530

    My first Commit

---
+++ src/main/java/com/example/springboot/FirstSpringBootApllicationToInsertCsvDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootApllicationToInsertCsvDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootApllicationToInsertCsvDataApplication.class, args);
+	}
+
+}
